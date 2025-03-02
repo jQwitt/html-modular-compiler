f46f7e0012b59609eb8f48258ff02f7d4f683c21
jwitt <jwitt1452@gnail.com>
2025-03-01 17:12:08 -0800

    fix: format buildComponentMap

--- src/lib/helpers/buildComponentMap.ts
+++ src/lib/helpers/buildComponentMap.ts
@@ -1,28 +1,30 @@
-import { COMPONENTS_DIR, decoder } from "../index.ts";
-import { HTMLComponentMap } from "../types.ts";
-import { parseAttributes } from "./parser/index.ts";
+import { COMPONENTS_DIR, decoder } from '../index.ts';
+import { HTMLComponentMap } from '../types.ts';
+import { parseTopLevelAttributes } from './parser/index.ts';
 
 const buildComponentMap = () => {
-  const components = Deno.readDirSync(COMPONENTS_DIR);
-  const map: HTMLComponentMap = {};
+	const components = Deno.readDirSync(COMPONENTS_DIR);
+	const map: HTMLComponentMap = {};
 
-  for (const { name } of components) {
-    const key = name.split(".")[0].trim(); // format: [name].html
-    if (map[key]) {
-      throw new Error(`Duplicate component name: ${name}`);
-    }
+	for (const { name } of components) {
+		const key = name.split('.')[0].trim(); // format: [name].html
+		if (map[key]) {
+			throw new Error(`Duplicate component name: ${name}`);
+		}
 
-    const base = decoder.decode(Deno.readFileSync(`${COMPONENTS_DIR}/${name}`));
-    const attributes = parseAttributes(base);
+		const base = decoder.decode(
+			Deno.readFileSync(`${COMPONENTS_DIR}/${name}`),
+		);
+		const attributes = parseTopLevelAttributes(base);
 
-    map[key] = {
-      name: key,
-      base,
-      attributes,
-    };
-  }
+		map[key] = {
+			name: key,
+			base,
+			attributes,
+		};
+	}
 
-  return map;
+	return map;
 };
 
 export default buildComponentMap;
